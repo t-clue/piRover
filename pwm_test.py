a5629dc8a747c2e6d8a65b49c73cfd7fd2fdb68a
松島亮輔 <m.ryosuke@me.com>
2020-03-26 16:12:04 +0900

    [modify] add set_mode

--- pwm_test.py
+++ pwm_test.py
@@ -8,14 +8,20 @@ gpio_stanby = 8
 
 pi = pigpio.pi()
 pi.set_mode(gpio_pwma, pigpio.OUTPUT)
+pi.set_mode(gpio_ain1, pigpio.OUTPUT)
+pi.set_mode(gpio_ain2, pigpio.OUTPUT)
+pi.set_mode(gpio_stanby, pigpio.OUTPUT)
+
 pi.write(gpio_stanby, 1)
 pi.write(gpio_ain1, 0)
 pi.write(gpio_ain2, 1)
 
-# 2Hz、duty比0.5
 pi.hardware_PWM(gpio_pin0, 2, 500000)
 
 time.sleep(5)
 
 pi.set_mode(gpio_pwma, pigpio.INPUT)
+pi.set_mode(gpio_ain1, pigpio.INPUT)
+pi.set_mode(gpio_ain2, pigpio.INPUT)
+pi.set_mode(gpio_stanby, pigpio.INPUT)
 pi.stop()
