5db480b097f7d30e9dc3765ac8b4d406662096ae
RyosukeMatsushima <m.ryosuke@me.com>
2020-11-27 14:45:07 +0900

    fix caluculation degree to duty cycle

--- ServoDriver.py
+++ ServoDriver.py
@@ -15,7 +15,7 @@ class ServoDriver:
     def set_degree(self, degree):
         if not(0 <= degree <= 180):
             TypeError("Range of degree is 0...180")
-        degree = degree / 180 * 2.0 + 0.5 # servo data sheet https://akizukidenshi.com/download/ds/towerpro/SG90_a.pdf
+        degree = (degree / 180 * 1.9 + 0.5) / 20 * 100 # servo data sheet https://akizukidenshi.com/download/ds/towerpro/SG90_a.pdf
         value = int(degree * 10 ** 4)
         self.pi.hardware_PWM(self._pin, 50, value)
 
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     gpio_pwm = 19
 
     driver = ServoDriver(gpio_pwm)
-    degree_list = [0, 60, 60, 120, 180]
+    degree_list = [0, 60, 90, 120, 180]
     for degree in degree_list:
         driver.set_degree(degree)
         time.sleep(1)
